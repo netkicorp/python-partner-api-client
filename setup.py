20ff34f12812b4b7f77dfc207a9d0ad4d5e96b37
Frank Contreras <frank@netki.com>
2017-02-21 16:19:19 -0800

    - Tweaking setup.py to prepare for PyPi

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup
 
+install_requires = [
+    'attrdict==2.0.0',
+    'ecdsa==0.13',
+    'pyOpenSSL==16.0.0',
+    'requests==2.7.0',
+    'six==1.9.0',
+    'wsgiref==0.1.2'
+]
+
+test_requires = [
+    'mock==1.0.1'
+]
+
 setup(
     name='netki-partner-client',
     packages=['netki-partner-client'],
@@ -8,8 +21,8 @@ setup(
     author='Netki Opensource',
     author_email='opensource@netki.com',
     url='https://github.com/netkicorp/python-partner-api-client',
-    download_url='',
     keywords=['netki', 'partner', 'certificate', 'walletname', 'wallet', 'name', 'bitcoin', 'blockchain', 'identity'],
-    classifiers=[],
     license='BSD',
+    install_requires=install_requires,
+    test_requires=test_requires
 )
